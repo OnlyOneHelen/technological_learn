2cf82f847f72913719ca4470163757da9c7e4e92
helen <582279617@qq.com>
2021-05-14 17:16:32 +0800

    提交更新

---
+++ src/views/cssList/6.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div>(权重)!important最高 如果这个!important用于简写的样式属性，会导致所代表的子属性都作用与这个属性 性能比较差</div>
+        <div>内联样式和外联样式的优先级和加载顺序有关，因为css样式是单线程的从上向下加载</div>
+        <div>Important > 行内样式 > 内联样式和外联样式</div>
+    </div>
+</template>
--- src/views/cssList/index.vue
+++ src/views/cssList/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="about">
     <number-one></number-one>
+    <number-two></number-two>
+    <number-four></number-four>
+    <number-six></number-six>
+    <number-eight></number-eight>
   </div>
 </template>
 
 <script>
 import numberOne from './1.vue'
+import numberTwo from './2.vue'
+import numberFour from './4.vue'
+import numberSix from './6.vue'
+import numberEight from './8.vue'
 export default {
   data () {
     return {
@@ -13,7 +21,16 @@ export default {
     }
   },
   components: {
-    numberOne
+    numberOne,
+    numberTwo,
+    numberFour,
+    numberSix,
+    numberEight
   }
 }
 </script>
+<style>
+    .about>div {
+        border: 1px solid #ccc;
+    }
+</style>
